94d6c8f5b0f215da3a8d6263f2ad7273517a6d7a
Lipien <adam@pawtel.pl>
2019-10-27 09:26:29 +0100

    Example with application profiles (default, dev).

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,9 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-k
-;src/main/java/pl/javastart/beans/SimpleMessageProducer.java,a\5\a5b8c44b4bdfb4327db26d3f73f1d50575bc8c88
 c
 3src/main/java/pl/javastart/SpringDiApplication.java,d\7\d78fd0454e365e63d82bb63f7e496738dc83faff
 d
 4src/main/java/pl/javastart/beans/MessagePrinter.java,a\c\ac044315c0d466014391ff44576dc40de68ef915
-o
-?src/main/java/pl/javastart/beans/UpperCaseMessageDecorator.java,5\2\52eb75355962b1f274b3fe170d7d58b2956ecb53
-f
-6src/main/java/pl/javastart/beans/MessageDecorator.java,5\d\5d54ff3e4836131b57f01418bf07e3062a7e08be
+e
+5src/main/java/pl/javastart/beans/MessageProducer.java,6\f\6fb2c437df13e2f9113e56a071be671b6836fea0
--- src/main/java/pl/javastart/SpringDiApplication.java
+++ src/main/java/pl/javastart/SpringDiApplication.java
@@ -3,18 +3,20 @@ package pl.javastart;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import pl.javastart.beans.MessagePrinter;
+import pl.javastart.beans.NamesRepository;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan
 public class SpringDiApplication {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringDiApplication.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+                SpringDiApplication.class);
 
-        MessagePrinter bean1 = ctx.getBean(MessagePrinter.class);
-        bean1.printMessage();
-        MessagePrinter bean2 = ctx.getBean(MessagePrinter.class);
-        bean2.printMessage();
+        NamesRepository namesRepo= ctx.getBean(NamesRepository.class);
+        List <String> allNames = namesRepo.getAll();
+        allNames.forEach(System.out::println);
 
         ctx.close();
     }
