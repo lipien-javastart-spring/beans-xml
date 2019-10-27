a2e2127d0f2890a75ef3ec27b2b67c5473a0eb13
Lipien <adam@pawtel.pl>
2019-10-27 12:12:13 +0100

    Added bean @Profile default.

--- src/main/java/pl/javastart/beans/NamesRepository.java
+++ src/main/java/pl/javastart/beans/NamesRepository.java
@@ -1,15 +1,25 @@
 package pl.javastart.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.javastart.config.DatabaseDatasource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class NamesRepository {
 
+
     private DatabaseDatasource ds;
+
+    @Bean
+    @Profile("default")
+    public DatabaseDatasource getDefaultDatasource() {
+        return (() -> Arrays.asList("KasiaDefault", "BartekDefault"));
+    }
 
     @Autowired
     public NamesRepository(DatabaseDatasource ds) {
